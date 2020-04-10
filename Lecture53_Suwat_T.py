2cfcb563ea8c5f2466f9ca14a27fd7b4979ac10a
mosuwat <62417783+mosuwat@users.noreply.github.com>
2020-04-10 16:19:17 +0700

    Delete Lecture53_Suwat_T.py

--- Lecture53_Suwat_T.py
+++
@@ -1,6 +0,0 @@
-def VatCalulator(TotalPrice):
-    result = TotalPrice+(TotalPrice*7/100)
-    return float(result)
-
-Price = float(input("Enter price: "))
-print("You have to pay",VatCalulator(Price),"THB")
